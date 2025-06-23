2b18c630c691514ef34e114ebe300732ee16d762
samidejen <bold.3453a@gmail.com>
2025-06-23 08:58:26 -0400

    Update auth.route.js

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,7 +1,13 @@
 import express from "express";
-import { login, logout, onboard, signup } from "../controllers/auth.controller.js";
+import {
+  login,
+  logout,
+  onboard,
+  signup,
+  uploadPhoto,
+} from "../controllers/auth.controller.js";
 import { protectRoute } from "../middleware/auth.middleware.js";
-
+import { uploadProfile } from "../lib/cloudinaryProfile.js";
 const router = express.Router();
 
 router.post("/signup", signup);
@@ -14,5 +20,5 @@ router.post("/onboarding", protectRoute, onboard);
 router.get("/me", protectRoute, (req, res) => {
   res.status(200).json({ success: true, user: req.user });
 });
-
+router.route("/uploadprofile").post(protectRoute, uploadProfile, uploadPhoto);
 export default router;
