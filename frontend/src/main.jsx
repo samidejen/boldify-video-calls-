d08797452c7e0beeeda947e5f4b9266845633ca3
samidejen <bold.3453a@gmail.com>
2025-06-23 10:15:39 -0400

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,9 +3,7 @@ import { createRoot } from "react-dom/client";
 import "stream-chat-react/dist/css/v2/index.css";
 import "./index.css";
 import App from "./App.jsx";
-
 import { BrowserRouter } from "react-router";
-
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const queryClient = new QueryClient();
