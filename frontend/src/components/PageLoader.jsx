d517a470bc44ae44b0a99d2edd6632a31585ba19
samidejen <bold.3453a@gmail.com>
2025-06-23 09:39:49 -0400

    Update PageLoader.jsx

--- frontend/src/components/PageLoader.jsx
+++ frontend/src/components/PageLoader.jsx
@@ -4,7 +4,10 @@ import { useThemeStore } from "../store/useThemeStore";
 const PageLoader = () => {
   const { theme } = useThemeStore();
   return (
-    <div className="min-h-screen flex items-center justify-center" data-theme={theme}>
+    <div
+      className="min-h-screen flex items-center justify-center"
+      data-theme={theme}
+    >
       <LoaderIcon className="animate-spin size-10 text-primary" />
     </div>
   );
