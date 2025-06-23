fe388154f226d808c8616d558d5dcd84f81878bf
samidejen <bold.3453a@gmail.com>
2025-06-23 08:48:05 -0400

    Update User.js

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -64,7 +64,10 @@ userSchema.pre("save", async function (next) {
 });
 
 userSchema.methods.matchPassword = async function (enteredPassword) {
-  const isPasswordCorrect = await bcrypt.compare(enteredPassword, this.password);
+  const isPasswordCorrect = await bcrypt.compare(
+    enteredPassword,
+    this.password
+  );
   return isPasswordCorrect;
 };
 
