3b68a300522a74548f2889a1ef0d3c458c2f4b6e
samidejen <bold.3453a@gmail.com>
2025-06-23 10:13:56 -0400

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,20 @@
 import { Navigate, Route, Routes } from "react-router";
-
 import HomePage from "./pages/HomePage.jsx";
+
 import SignUpPage from "./pages/SignUpPage.jsx";
 import LoginPage from "./pages/LoginPage.jsx";
 import NotificationsPage from "./pages/NotificationsPage.jsx";
 import CallPage from "./pages/CallPage.jsx";
 import ChatPage from "./pages/ChatPage.jsx";
 import OnboardingPage from "./pages/OnboardingPage.jsx";
-
+import PageLoader from "./components/PageLoader.jsx";
+import Layout from "./components/Layout.jsx";
 import { Toaster } from "react-hot-toast";
 
-import PageLoader from "./components/PageLoader.jsx";
-import useAuthUser from "./hooks/useAuthUser.js";
-import Layout from "./components/Layout.jsx";
 import { useThemeStore } from "./store/useThemeStore.js";
+import useAuthUser from "./hooks/useAuthUser.js";
 
-const App = () => {
+export default function App() {
   const { isLoading, authUser } = useAuthUser();
   const { theme } = useThemeStore();
 
@@ -25,7 +24,7 @@ const App = () => {
   if (isLoading) return <PageLoader />;
 
   return (
-    <div className="h-screen" data-theme={theme}>
+    <div className="min-h-screen" data-theme={theme}>
       <Routes>
         <Route
           path="/"
@@ -42,13 +41,21 @@ const App = () => {
         <Route
           path="/signup"
           element={
-            !isAuthenticated ? <SignUpPage /> : <Navigate to={isOnboarded ? "/" : "/onboarding"} />
+            !isAuthenticated ? (
+              <SignUpPage />
+            ) : (
+              <Navigate to={isOnboarded ? "/" : "/onboarding"} />
+            )
           }
         />
         <Route
           path="/login"
           element={
-            !isAuthenticated ? <LoginPage /> : <Navigate to={isOnboarded ? "/" : "/onboarding"} />
+            !isAuthenticated ? (
+              <LoginPage />
+            ) : (
+              <Navigate to={isOnboarded ? "/" : "/onboarding"} />
+            )
           }
         />
         <Route
@@ -73,7 +80,6 @@ const App = () => {
             )
           }
         />
-
         <Route
           path="/chat/:id"
           element={
@@ -86,7 +92,6 @@ const App = () => {
             )
           }
         />
-
         <Route
           path="/onboarding"
           element={
@@ -102,9 +107,7 @@ const App = () => {
           }
         />
       </Routes>
-
       <Toaster />
     </div>
   );
-};
-export default App;
+}
