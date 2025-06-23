657434809f2b5b0978b3757d0dbd26f690f9f1c0
samidejen <bold.3453a@gmail.com>
2025-06-23 09:50:00 -0400

    Update useUploadProfilePhoto.js

--- frontend/src/hooks/useUploadProfilePhoto.js
+++ frontend/src/hooks/useUploadProfilePhoto.js
@@ -1,1 +1,25 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { axiosInstance } from "../lib/axios";
+import toast from "react-hot-toast";
 
+export const useUploadProfilePhoto = () => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: async (file) => {
+      const formData = new FormData();
+      formData.append("profilePic", file);
+      const res = await axiosInstance.post("/auth/uploadprofile", formData);
+
+      return res.data;
+    },
+    onSuccess: () => {
+      toast.success("Profile photo updated successfully!");
+
+      queryClient.invalidateQueries({ queryKey: ["authUser"] });
+    },
+    onError: () => {
+      toast.error("Failed to upload profile photo");
+    },
+  });
+};
